16cd2338902b9f2ac3d6d9348be0c041a3072a4b
AmanTadele <amantadele3@gmail.com>
2022-07-06 13:16:37 -0700

    check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
  */
-int _putchar(char c)
+int _islower(int c)
 {
-	return (write(1, &c, 1));
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
 }
